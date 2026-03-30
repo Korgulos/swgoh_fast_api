f997ad6f9cf9ccf17c56e551a619f24f2d00cefc
Dejan Katic <katicod100@gmail.com>
2026-03-30 16:21:59 +0200

    crud raper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import psycopg2
 from dotenv import load_dotenv
@@ -6,6 +7,15 @@ import os
 
 load_dotenv()
 app = FastAPI()
+
+# 👉 OVDE IDE CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ili konkretan domen
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 📦 MODELI
 
